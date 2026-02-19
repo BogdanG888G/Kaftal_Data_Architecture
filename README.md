2c2e6dbd8d3dcf81f358d3f84f3b27cc16b94695
Bogdan <BogdanG888G@yandex.ru>
2026-02-19 18:20:56 +0300

    Обновил docker-compose, подключил spark и настроил file sensor в airflow

--- README.md
+++ README.md
@@ -19,3 +19,30 @@ Iceberg staging → Iceberg core (трансформации в SQL)
     │
     └─→ (опционально) MS SQL Server (если нужен для Power BI)
 ```
+
+```
+graph TD
+    A[Источники: Файлы (CSV/XLSX)] -->|Airflow (сенсоры)| B(S3 Landing)
+    G[Kafka (События)] -->|Spark Struct. Streaming| C
+    
+    B -->|Airflow -> Spark| C(Iceberg Raw - S3)
+    C -->|dbt test / Great Expectations| D{Quality Check}
+    D -- OK --> E[dbt run via Trino]
+    D -- Fail --> F[Alert in Slack/Telegram]
+    
+    E --> G[(Iceberg Core / Features)]
+    
+    G --> H[Trino (Ad-hoc)]
+    G --> I[Airflow -> ClickHouse (Mart Update)]
+    G --> J[MLflow (Log features)]
+    
+    I --> K[(ClickHouse Marts)]
+    K --> L[Superset / Grafana]
+    
+    H --> M[FastAPI (Data API)]
+    M --> N[JSON for external apps]
+    
+    J --> O[MLflow Tracking Server]
+    
+    G -.-> P[(Greenplum - Archive/Heavy loads)]
+```
--- dags/etl_data_transformation.py
+++ dags/etl_data_transformation.py
@@ -1,11 +1,13 @@
 from airflow.decorators import task, dag
-#from airflow.providers.
+from airflow.providers.amazon.aws.hooks.s3 import S3Hook
+from airflow.providers.amazon.aws.sensors.s3 import S3KeySensor
+from airflow.providers.standard.sensors.filesystem import FileSensor
 
 import logging
 import pandas as pd
 
 from datetime import *
-
+import os
 
 default_args = {
     'owner': 'airflow',
@@ -14,20 +16,52 @@ default_args = {
 
 @dag(
     dag_id = 'etl_transformation',
-    description = 'Перевод файлов хранилище S3',
+    description = 'Перевод файлов в хранилище S3',
     catchup = False,
     start_date = datetime(2026, 2, 1),
     schedule = '@weekly',
     tags = ['s3']
 )
 
+
 def pipeline():
+
+
+    wait_for_data = FileSensor(
+        task_id = 'wait_for_data', 
+        fs_conn_id = 'folder_connect',
+        filepath = '/opt/airflow/data/*.csv',
+        mode = 'reschedule',
+        poke_interval = 20,
+        timeout = 60
+        )
+
 
     @task
     def start():
 
-        logging.info('Запуск программы')
+        data_dir = '/opt/airflow/data'
 
-    start()
+        s3 = S3Hook(aws_conn_id = 's3_connect')
+        
+        folder = os.listdir(data_dir)
+
+        for i in folder:
+            
+            parts = i.split('_')
+            
+            ans = parts[2][:-4] + '/' + parts[1] + '/' + parts[0]
+
+
+            s3.load_file(
+                filename = data_dir + '/' + i,
+                key = ans,
+                bucket_name = 'data',
+                replace = True)
+            
+            logging.info(f'Загрузили файл {i}')
+
+    wait_for_data >> start()
 
 pipeline()
+
